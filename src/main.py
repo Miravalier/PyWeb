f21d49c739175d59bbdfe6c9590e011456133f42
Miravalier <benjamin@miramontes.dev>
2025-11-21 17:20:11 -0600

    Added some example python to demonstrate using an event listener

--- src/main.py
+++ src/main.py
@@ -1,6 +1,16 @@
+from pyodide.ffi import create_proxy
 from js import document
 
 
+# The @create_proxy decorator allows this function to be used as
+# an event handler from JS code
+@create_proxy
+def on_click(event):
+    print("Text Clicked:", event.target.textContent)
+
+
+# This gets called once the page finished loading
 def main():
     container = document.body.appendChild(document.createElement("div"))
     container.textContent = "Example Text"
+    container.addEventListener("click", on_click)
